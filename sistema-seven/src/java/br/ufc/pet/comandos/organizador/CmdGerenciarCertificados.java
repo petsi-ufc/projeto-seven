af76dc6e2506ec694fd417c2566c52f70309d36d
junior.hpj <junior.hpj@hotmail.com>
2014-07-30 13:44:40 +0000

    Foi criado um link para gerenciar emissão de certificados, e uma página para tal gerenciamento.

---
+++ sistema-seven/src/java/br/ufc/pet/comandos/organizador/CmdGerenciarCertificados.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufc.pet.comandos.organizador;
+
+import br.ufc.pet.evento.Evento;
+import br.ufc.pet.interfaces.Comando;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author junior
+ */
+public class CmdGerenciarCertificados implements Comando{
+
+    public String executa(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        
+        Evento evento = (Evento) session.getAttribute("evento");
+
+        return "/org/organ_gerenciar_certificados.jsp";
+
+    }
+
+}

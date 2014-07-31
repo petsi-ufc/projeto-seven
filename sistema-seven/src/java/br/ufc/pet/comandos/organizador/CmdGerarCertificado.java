091d88836bda6f118ac4c46d75c2274605c26a97
junior.hpj <junior.hpj@hotmail.com>
2014-07-31 11:30:58 +0000

    Foi realizado o calculo da carga horária de cada atividade.

--- sistema-seven/src/java/br/ufc/pet/comandos/organizador/CmdGerarCertificado.java
+++ sistema-seven/src/java/br/ufc/pet/comandos/organizador/CmdGerarCertificado.java
@@ -2,13 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.ufc.pet.comandos.organizador;
 
 import br.ufc.pet.evento.Atividade;
+import br.ufc.pet.evento.Horario;
 import br.ufc.pet.evento.Inscricao;
 import br.ufc.pet.evento.Perfil;
 import br.ufc.pet.interfaces.Comando;
+import br.ufc.pet.services.HorarioService;
 import br.ufc.pet.services.InscricaoService;
 import br.ufc.pet.util.UtilSeven;
 import com.lowagie.text.Document;
@@ -38,12 +39,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 /**
  *
  * @author mardson
  */
-public class CmdGerarCertificado implements Comando{
+public class CmdGerarCertificado implements Comando {
 
     @Override
     public String executa(HttpServletRequest request, HttpServletResponse response) {
@@ -52,70 +52,82 @@ public class CmdGerarCertificado implements Comando{
 
         String inscricao_id = request.getParameter("insc_id");
 
-        if(inscricao_id.trim().isEmpty() || inscricao_id == null){
+        if (inscricao_id.trim().isEmpty() || inscricao_id == null) {
             session.setAttribute("erro", "inscrição inválida!");
             return "/org/organ_gerenciar_inscricoes.jsp";
-        }else{
+        } else {
             InscricaoService is = new InscricaoService();
             Inscricao inscricao = is.getInscricaoById(Long.parseLong(inscricao_id));
 
-             response.setContentType("application/pdf");
+            response.setContentType("application/pdf");
             response.setHeader("Content-Disposition", " attachment; filename=\"relatorio_" + inscricao.getParticipante().getUsuario().getNome() + ".pdf\"");
 
-            
+
             Document document = new Document(PageSize.LETTER.rotate());
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            
-            try{
-            PdfWriter writer = PdfWriter.getInstance(document, baos);
-            document.open();
-            //document.setPageSize(PageSize.A4);
 
-            
-            for(Atividade a:inscricao.getAtividades()){
-                
-            
-
-            Image jpgTemplate = Image.getInstance("http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+"/imagens/template.jpg");
-            
-            
-            PdfContentByte canvas = writer.getDirectContentUnder();
-            jpgTemplate.scaleAbsolute(document.getPageSize().getWidth(), document.getPageSize().getHeight());
-            jpgTemplate.setAbsolutePosition(0, 0);
-            canvas.addImage(jpgTemplate);
-                        
-
-            Paragraph cert2 = new Paragraph(" ", FontFactory.getFont(FontFactory.HELVETICA, 30, Font.BOLD));
-            cert2.setAlignment(Element.ALIGN_CENTER);
-            cert2.setSpacingBefore(10);
-            cert2.setSpacingAfter(15);
-            document.add(cert2);
-
-            Paragraph cert = new Paragraph(inscricao.getParticipante().getUsuario().getNome(), FontFactory.getFont(FontFactory.HELVETICA, 27, Font.BOLD));
-            cert.setAlignment(Element.ALIGN_CENTER);
-            cert.setSpacingBefore(150);
-            cert.setSpacingAfter(45);
-            document.add(cert);
-
-            Paragraph p1 = new Paragraph(a.getNome(), FontFactory.getFont(FontFactory.HELVETICA, 27, Font.BOLD));
-            p1.setAlignment(Element.ALIGN_CENTER);
-            document.add(p1);
+            try {
+                PdfWriter writer = PdfWriter.getInstance(document, baos);
+                document.open();
+                //document.setPageSize(PageSize.A4);
 
 
-           
-            Paragraph p4 = new Paragraph("    Carga horária: " + a.getCargaHoraria() + " horas", FontFactory.getFont(FontFactory.HELVETICA, 18, Font.BOLD));
-            p4.setSpacingBefore(52);
-            
-            document.add(p4);
-            
-            document.newPage();
+                for (Atividade a : inscricao.getAtividades()) {
 
-            }
+                    Image jpgTemplate = Image.getInstance("http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/imagens/template.jpg");
 
-            document.close();
 
-            }catch(Exception ex){
-              ex.printStackTrace();
+                    PdfContentByte canvas = writer.getDirectContentUnder();
+                    jpgTemplate.scaleAbsolute(document.getPageSize().getWidth(), document.getPageSize().getHeight());
+                    jpgTemplate.setAbsolutePosition(0, 0);
+                    canvas.addImage(jpgTemplate);
+
+
+                    Paragraph cert2 = new Paragraph(" ", FontFactory.getFont(FontFactory.HELVETICA, 30, Font.BOLD));
+                    cert2.setAlignment(Element.ALIGN_CENTER);
+                    cert2.setSpacingBefore(10);
+                    cert2.setSpacingAfter(15);
+                    document.add(cert2);
+
+                    Paragraph cert = new Paragraph(inscricao.getParticipante().getUsuario().getNome(), FontFactory.getFont(FontFactory.HELVETICA, 27, Font.BOLD));
+                    cert.setAlignment(Element.ALIGN_CENTER);
+                    cert.setSpacingBefore(150);
+                    cert.setSpacingAfter(45);
+                    document.add(cert);
+
+                    Paragraph p1 = new Paragraph(a.getNome(), FontFactory.getFont(FontFactory.HELVETICA, 27, Font.BOLD));
+                    p1.setAlignment(Element.ALIGN_CENTER);
+                    document.add(p1);
+
+                    HorarioService hs = new HorarioService();
+                    ArrayList<Horario> horarios = hs.getHorariosByAtivideId(a.getId());
+
+                    double cargaHoraria = 0;
+
+                    for(Horario horario: horarios){
+                        //Calculo da Carga horaria da aividade
+                        //((HoraFinal - HoraInicial)*60(converte para minutos)) + (minFinal - MinInicial)
+                        int minutos = ((horario.getHoraFinal() - horario.getHoraInicial())*60) + (horario.getMinutoFinal() - horario.getMinutoInicial());
+
+                        cargaHoraria = cargaHoraria + minutos;
+
+                    }
+
+                    cargaHoraria = cargaHoraria/60;
+
+                    Paragraph p4 = new Paragraph("    Carga horária: " + cargaHoraria + " horas.", FontFactory.getFont(FontFactory.HELVETICA, 18, Font.BOLD));
+                    p4.setSpacingBefore(52);
+
+                    document.add(p4);
+
+                    document.newPage();
+
+                }
+
+                document.close();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
                 session.setAttribute("erro", "Erro " + ex.getMessage());
                 return "/org/organ_gerenciar_inscricoes.jsp";
             }
@@ -143,7 +155,5 @@ public class CmdGerarCertificado implements Comando{
         }
 
         return "/org/organ_gerenciar_inscricoes.jsp";
-}
-    
-
+    }
 }

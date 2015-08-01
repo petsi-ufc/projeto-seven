0f3e347e343b332a828bc3eaf7f3a18a8763fa18
andersonlemosufc <andersonlemos.ufc@gmail.com>
2015-08-01 12:15:18 -0300

    finalizada a parte de validação do certificado

--- sistema-seven/src/java/br/ufc/pet/comandos/CmdValidarDocumento.java
+++ sistema-seven/src/java/br/ufc/pet/comandos/CmdValidarDocumento.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufc.pet.comandos;
 
 import br.ufc.pet.evento.Atividade;
@@ -11,14 +7,12 @@ import br.ufc.pet.interfaces.Comando;
 import br.ufc.pet.services.AtividadeService;
 import br.ufc.pet.services.InscricaoService;
 import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Escritorio projetos
+/*
+ * @author Anderson
  */
 public class CmdValidarDocumento implements Comando {
 
